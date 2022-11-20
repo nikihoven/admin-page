809945bb9f92477d4f868f31878942f083fef948
Nikita Lukashik <n.lukashik@vsemayki.ru>
2022-11-20 19:16:07 +0300

    Installed ESLint, husky and lint-staged; Applied ESLint code style rules; Added pre-commit husky hook; Created .npmrc config;

--- src/App.tsx
+++ src/App.tsx
@@ -25,79 +25,79 @@ interface responseDataToShow {
 }
 
 const App = () => {
-    const [resData, setResData] = useState<responseDataToShow>()
-    const getStatusInputRef = useRef<HTMLInputElement>(null)
-    const linksToQueueInputRef = useRef<HTMLInputElement>(null)
+	const [resData, setResData] = useState<responseDataToShow>()
+	const getStatusInputRef = useRef<HTMLInputElement>(null)
+	const linksToQueueInputRef = useRef<HTMLInputElement>(null)
 
-    const getQueueStatus = async (event: FormEvent<HTMLFormElement>) => {
-        event.preventDefault()
+	const getQueueStatus = async (event: FormEvent<HTMLFormElement>) => {
+		event.preventDefault()
 
-        const id = getStatusInputRef.current?.value
+		const id = getStatusInputRef.current?.value
 
-        if (!id) return
+		if (!id) return
 
-        try {
-            const {data: {data}} = await axiosInstance.get<getQueueStatusResponse>(`/tasks/previews/${id}`)
+		try {
+			const {data: {data}} = await axiosInstance.get<getQueueStatusResponse>(`/tasks/previews/${id}`)
 
-            setResData({
-                id: data.task_id,
-                status: data.task_status,
-                result: data.task_result,
-                isError: false
-            })
-        } catch (e) {
-            setResData({isError: true})
-        }
-    }
+			setResData({
+				id: data.task_id,
+				status: data.task_status,
+				result: data.task_result,
+				isError: false
+			})
+		} catch (e) {
+			setResData({isError: true})
+		}
+	}
 
-    const setLinksToQueue = async (event: FormEvent<HTMLFormElement>) => {
-        event.preventDefault()
+	const setLinksToQueue = async (event: FormEvent<HTMLFormElement>) => {
+		event.preventDefault()
 
-        const links = getStatusInputRef.current?.value.split(' ').filter(el => !!el)
+		const links = getStatusInputRef.current?.value.split(' ').filter(el => !!el)
 
-        if (!links?.length) return
+		if (!links?.length) return
 
-        try {
-            const {data: {data}} = await axiosInstance.post<setLinksToQueueResponse>('/previews', JSON.stringify(links))
+		try {
+			const {data: {data}} = await axiosInstance.post<setLinksToQueueResponse>('/previews', JSON.stringify(links))
 
-            setResData({
-                id: data.task_id,
-                isError: false
-            })
-        } catch (e) {
-            setResData({isError: true})
-        }
-    }
+			setResData({
+				id: data.task_id,
+				isError: false
+			})
+		} catch (e) {
+			setResData({isError: true})
+		}
+	}
 
 
-    return (
-        <main className="main">
-            <section className="main__half">
-                <h1 className="heading">Админ панель</h1>
-                <form onSubmit={getQueueStatus} className="form">
-                    <label className="form__label">Получить статус очереди
-                        <input ref={getStatusInputRef} className="form__input" placeholder="Введите id" type="text"/>
-                    </label>
-                    <button className="form__button">Получить статус</button>
-                </form>
-                <form onSubmit={setLinksToQueue} className="form">
-                    <label className="form__label">Поставить ссылки в очередь
-                        <input ref={linksToQueueInputRef} className="form__input"
-                               placeholder="Введите список ссылок через пробелы" type="text"/>
-                    </label>
-                    <button className="form__button">Отправить в очередь</button>
-                </form>
-            </section>
+	return (
+		<main className="main">
+			<section className="main__half">
+				<h1 className="heading">Админ панель</h1>
+				<form onSubmit={getQueueStatus} className="form">
+					<label className="form__label">Получить статус очереди
+						<input ref={getStatusInputRef} className="form__input" placeholder="Введите id" type="text"/>
+					</label>
+					<button className="form__button">Получить статус</button>
+				</form>
+				<form onSubmit={setLinksToQueue} className="form">
+					<label className="form__label">Поставить ссылки в очередь
+						<input ref={linksToQueueInputRef} className="form__input"
+							placeholder="Введите список ссылок через пробелы" type="text"/>
+					</label>
+					<button className="form__button">Отправить в очередь</button>
+				</form>
+			</section>
 
-            <section className="main__half">
-                { resData?.isError !== undefined && <h2>{resData?.isError ? 'Произошла ошибка' : 'Результат:'}</h2>
-                }
-                {resData?.id && <p>ID: {resData.id}</p>}
-                {resData?.status && <p>STATUS: {resData.status}</p>}
-                {resData?.result && <p>RESULT: {resData.result}</p>}
-            </section>
-        </main>
-    )
+			<section className="main__half">
+				{ resData?.isError !== undefined && <h2>{resData?.isError ? 'Произошла ошибка' : 'Результат:'}</h2>
+				}
+				{resData?.id && <p>ID: {resData.id}</p>}
+				{resData?.status && <p>STATUS: {resData.status}</p>}
+				{resData?.result && <p>RESULT: {resData.result}</p>}
+			</section>
+		</main>
+	)
 }
 
 export {App}
--- src/index.tsx
+++ src/index.tsx
@@ -5,5 +5,5 @@ import {App} from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <App/>
+	<App/>
 )
